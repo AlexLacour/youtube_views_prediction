b6ec8b33052d9beb6bb2d0259fcb93c7b0ac8572
lacour_a <alexandre.lacour3@hotmail.fr>
2019-12-28 19:57:35 +0100

    REEEEEEEE

--- scraper/app.py
+++ scraper/app.py
@@ -1,22 +1,15 @@
-from flask import Flask, redirect
+from flask import Flask
 import myScraper as scrap
-import requests
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-
-# @app.route('/scrap')
-# def getFeatures():
-#     
-#     # r = requests.post('http://127.0.0.1:5000/ml', dataScraped)
-#     return dataScraped
-
-
-# app.run(port=5000)
-
-app=Flask(__name__)
 
 @app.route('/')
 def getFeatures():
-    dataScraped = scrap.getFeatures('https://www.youtube.com/watch?v=Ugs9HASX4rA')
+    dataScraped = scrap.getFeatures(
+        'https://www.youtube.com/watch?v=Ugs9HASX4rA')
     return dataScraped
+
+
+if(__name__ == '__main__'):
+    app.run(host='0.0.0.0')
